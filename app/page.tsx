b962c0cb1f724237a013efc7b0b610c2b9388876
Raon98 <cheol1998@kakao.com>
2024-07-27 18:18:55 +0900

    feat : page router

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-
 export default function Page() {
   return (
     <>
-    <main className="flex min-h-screen flex-col p-6">
-      MAIN PAGE
-    </main>
+      <div className="flex min-h-screen flex-col p-6">MAIN PAGE</div>
     </>
   );
 }
